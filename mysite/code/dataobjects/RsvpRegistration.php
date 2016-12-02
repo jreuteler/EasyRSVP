07c3a45b2cb2a2812caf74fd3a4c749e097a64c2
Jan Reuteler <janreuteler@icloud.com>
2016-12-02 13:25:05 -0500

    added debugging for testing

--- mysite/code/dataobjects/RsvpRegistration.php
+++ mysite/code/dataobjects/RsvpRegistration.php
@@ -22,6 +22,8 @@ class RsvpRegistration extends DataObject
 
     public function getCMSFields()
     {
+        DBLogger::log('creating the registration fields', __METHOD__, SS_GENERAL);
+
         /*
          * create cms fields depending on the dynamically defined rsvp_fields, populate
          * them with the serialised data and lastly add a field with all the form values not defined
